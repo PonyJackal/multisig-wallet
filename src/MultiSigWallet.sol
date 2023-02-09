dd46c95a1fabe3dd5c8a2004c06c8a1ae48fdcf0
ponyjackal <pony1125@outlook.com>
2023-02-09 04:35:59 -0800

    feat: add transaction functions

--- src/MultiSigWallet.sol
+++ src/MultiSigWallet.sol
@@ -9,7 +9,7 @@ contract MultisigWallet is Ownable {
     mapping(address => bool) public isSignatory;
 
     struct Transaction {
-        address to;
+        address destination;
         uint value;
         bytes data;
         bool executed;
@@ -20,12 +20,22 @@ contract MultisigWallet is Ownable {
     mapping(uint256 => mapping(address => bool)) public isConfirmed;
 
     // transactions
-    mapping(uint256 => Transaction) public transactions;
+    Transaction[] public transactions;
 
     // events
     event SignatoryAdded(address indexed signatory);
     event SignatoryRemoved(address indexed signatory);
     event NumberOfRequiredSignatoriesUpdated(uint256 numOfRequiredSignatories);
+    event TransactionSubmitted(
+        address indexed signatory,
+        uint256 indexed nonce,
+        address indexed destination,
+        uint256 value,
+        bytes data
+    );
+    event TransactionConfirmed(address indexed signatory, uint256 indexed nonce);
+    event TransactionRevoked(address indexed signatory, uint256 indexed nonce);
+    event TransactionExecuted(address indexed signatory, uint256 indexed nonce);
 
     constructor(address[] memory _signatories, uint256 _numOfRequiredSignatories) {
         signatories = _signatories;
@@ -36,7 +46,13 @@ contract MultisigWallet is Ownable {
         }
     }
 
-    /** Owner functions */
+    /** Modifiers */
+    modifier onlySignatory() {
+        require(isSignatory[msg.sender], "Not a signatory");
+        _;
+    }
+
+    /** Owner Functions */
 
     /**
      * @dev add a new signatory
@@ -81,5 +97,82 @@ contract MultisigWallet is Ownable {
         numOfRequiredSignatories = _numOfRequiredSignatories;
 
         emit NumberOfRequiredSignatoriesUpdated(_numOfRequiredSignatories);
+    }
+
+    /** Mutative Functions */
+
+    /**
+     * @dev submit a transaction
+     * @param _destination The transaction destination address
+     * @param _value The transaction value
+     * @param _data The transaction data
+     */
+    function submitTransaction(address _destination, uint256 _value, bytes memory _data) external onlySignatory {
+        uint256 nonce = transactions.length;
+
+        transactions.push(
+            Transaction({
+                destination: _destination,
+                value: _value,
+                data: _data,
+                executed: false,
+                numOfConfirmations: 0
+            })
+        );
+
+        emit TransactionSubmitted(msg.sender, nonce, _destination, _value, _data);
+    }
+
+    /**
+     * @dev confirm a transaction
+     * @param _nonce The transaction destination address
+     */
+    function confirmTransaction(uint256 _nonce) external onlySignatory {
+        require(_nonce < transactions.length, "Transaction doesn't exist");
+        require(!isConfirmed[_nonce][msg.sender], "Already confirmed");
+
+        Transaction storage transaction = transactions[_nonce];
+        require(!transaction.executed, "Already executed");
+
+        isConfirmed[_nonce][msg.sender] = true;
+        transaction.numOfConfirmations++;
+
+        emit TransactionConfirmed(msg.sender, _nonce);
+    }
+
+    /**
+     * @dev revoke a transaction confirmation
+     * @param _nonce The transaction nonce
+     */
+    function revokeTransaction(uint256 _nonce) external onlySignatory {
+        require(_nonce < transactions.length, "Transaction doesn't exist");
+        require(isConfirmed[_nonce][msg.sender], "Not confirmed");
+
+        Transaction storage transaction = transactions[_nonce];
+        require(!transaction.executed, "Already executed");
+
+        isConfirmed[_nonce][msg.sender] = false;
+        transaction.numOfConfirmations--;
+
+        emit TransactionRevoked(msg.sender, _nonce);
+    }
+
+    /**
+     * @dev execute a transaction
+     * @param _nonce The transaction nonce
+     */
+    function executeTransaction(uint256 _nonce) external onlySignatory {
+        require(_nonce < transactions.length, "Transaction doesn't exist");
+
+        Transaction storage transaction = transactions[_nonce];
+        require(!transaction.executed, "Already executed");
+        require(transaction.numOfConfirmations >= numOfRequiredSignatories, "Cannot execute transaction");
+
+        transaction.executed = true;
+
+        (bool success, ) = transaction.destination.call{ value: transaction.value }(transaction.data);
+        require(success, "Transaction failed");
+
+        emit TransactionExecuted(msg.sender, _nonce);
     }
 }
